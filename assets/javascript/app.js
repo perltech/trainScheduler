42fa297fb6d2fc4b7bbf4e19c01ae979f3459ad0
perltech <noah.perlmutter@gmail.com>
2017-08-29 15:27:43 -0600

    store user input into values

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD8FzTeekZspVqYG429gUhWJkOZe0n2sks",
+  authDomain: "trainscheduler-608f9.firebaseapp.com",
+  databaseURL: "https://trainscheduler-608f9.firebaseio.com",
+  projectId: "trainscheduler-608f9",
+  storageBucket: "",
+  messagingSenderId: "844094080623"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+
+
+
+// Button to add new schedule
+$("#submit-button").on("click", function(event){
+  event.preventDefault();
+
+  // Store user input into variables
+  var trainName = $("#train-name-input").val();
+  var destination = $("#destination-input").val();
+  var initialTrainTime = $("#first-train-time-input").val();
+  var frequency = $("#frequency-input").val();
+  
+  console.log(trainName);
+  console.log(destination);
+  console.log(initialTrainTime);
+  console.log(frequency);
+
+})
+  
+
+}); // End of ready function
+
+
